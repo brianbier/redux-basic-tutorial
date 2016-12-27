a4b30f22dfa0dc5b89d6a80ccf24879f4f13fb9a
brianbier <briansbier@gmail.com>
2016-12-27 17:55:16 -0500

    add container that handles form

---
+++ src/containers/AddTodo.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { addTodo } from '../actions'
+
+let AddTodo = ({ dispatch }) => {
+  let input
+  return (
+    <div>
+      <form onSubmit={e => {
+        e.preventDefault()
+        if (!input.value.trim()) {
+          return
+        }
+        let user_input = input.value.trim()
+        let new_todo = addTodo(user_input)
+        dispatch(new_todo)
+        input.value = ''
+      }}>
+        <input ref={node => {
+          input = node
+        }} />
+        <button type="submit">
+          Add Todo
+        </button>
+      </form>
+    </div>
+  )
+}
+AddTodo = connect()(AddTodo)
+
+export default AddTodo
